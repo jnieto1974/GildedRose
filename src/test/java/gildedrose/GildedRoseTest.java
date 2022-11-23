4cbdd70c128a285927426de9e19139bea8542d0f
Jose Wenzel <devwenzel@gmail.com>
2022-11-23 18:30:15 +0100

    test: golden master of updateQuality

---
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -0,0 +1,25 @@
+package gildedrose;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GildedRoseTest {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/golden-master.input", delimiter = ';')
+    public void golden_master_testing(String itemName,
+                                      int initialSellIn,
+                                      int initialQuality,
+                                      int expectedSellIn,
+                                      int expectedQuality
+    ) {
+        Item item = new Item(itemName, initialSellIn, initialQuality);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.sellIn).isEqualTo(expectedSellIn);
+        assertThat(item.quality).isEqualTo(expectedQuality);
+    }
+}
